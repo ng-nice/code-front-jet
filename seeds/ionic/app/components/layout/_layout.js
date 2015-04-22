7cba70bb1d584ca1bc61e52f1872295c05b2cd66
asnowwolf <asnowwolf@gmail.com>
2015-04-22 12:18:00 +0800

    升版本号，以便发布

--- seeds/ionic/app/components/layout/_layout.js
+++ seeds/ionic/app/components/layout/_layout.js
@@ -2,10 +2,6 @@
 
 angular.module('app').controller('AppLayoutCtrl', function AppLayoutCtrl() {
   var vm = this;
-  var platform = vm.platformCss = {};
-  platform['platform-android'] = ionic.Platform.isAndroid();
-  platform['platform-ios'] = ionic.Platform.isIOS();
-  platform['platform-ipad'] = ionic.Platform.isIPad();
 });
 angular.module('app').directive('appLayout', function appLayout() {
   return {
